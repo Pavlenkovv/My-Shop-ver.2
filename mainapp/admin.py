3809634ccc09c6f22115459fa338c995861e639e
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-01-28 16:37:27 +0200

    add models notebook and smartphone

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,26 @@
+from django.forms import ModelChoiceField
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class NotebookAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CardProduct)
+admin.site.register(Card)
+admin.site.register(Customer)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,7 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-
+from django.db import models
 
 User = get_user_model()
 
@@ -15,6 +15,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    class Meta:
+        abstract = True
 
     category = models.ForeignKey(Category, verbose_name="Категорія", on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Найменування')
@@ -27,11 +29,39 @@ class Product(models.Model):
         return self.title
 
 
-class CardProduct(models.Model):
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Діагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255, verbose_name='Частота процесора')
+    ram = models.CharField(max_length=255, verbose_name="Оперативна пам'ять")
+    video = models.CharField(max_length=255, verbose_name='Відеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Чаз автономної роботи')
 
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Діагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Розширення екрану')
+    accum_volume = models.CharField(max_length=255, verbose_name='Об\'єм батареї')
+    ram = models.CharField(max_length=255, verbose_name="Оперативна пам'ять")
+    sd = models.BooleanField(default=True, verbose_name="Слот для карт пам\'яті")
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальний об\'єм карти пам\'яті')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Головна камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальна камера')
+
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
+
+
+class CardProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупець', on_delete=models.CASCADE)
     card = models.ForeignKey('Card', verbose_name='Кошик', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     quantity = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Загальна сума')
 
@@ -46,12 +76,10 @@ class Card(models.Model):
     final_price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Загальна вартість')
 
     def __str__(self):
-
         return str(self.id)
 
 
 class Customer(models.Model):
-
     user = models.ForeignKey(User, verbose_name='Покупець', on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Номер телефону')
     address = models.CharField(max_length=255, verbose_name='Адреса')
@@ -59,12 +87,11 @@ class Customer(models.Model):
     def __str__(self):
         return f'Покупець: {self.user.first_name} {self.user.last_name}'
 
-
-class Specification(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Ім'я товару для характеристик")
-
-    def __str__(self):
-        return f'Характеристики для товару: {self.name}'
+# class Specification(models.Model):
+#
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     name = models.CharField(max_length=255, verbose_name="Ім'я товару для характеристик")
+#
+#     def __str__(self):
+#         return f'Характеристики для товару: {self.name}'
