8610b8d9679600c6d457d892b7e0b3f71ca1218c
Pavlenkovv <pavlenko.vyacheslav@gmail.com>
2021-02-08 17:01:43 +0200

    tests done

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+from decimal import Decimal
+from unittest import mock
+from django.test import TestCase, RequestFactory
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+from .models import Category, Notebook, Cart, Customer, CartProduct
+from .views import recalc_cart, AddToCardView, BaseView
+
+
+User = get_user_model()
+
+
+class ShopTestCases(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(username='testuser', password='password')
+        self.category = Category.objects.create(name='Ноутбуки', slug='notebooks')
+        image = SimpleUploadedFile("notebook_image.jpg", content=b'', content_type="image/jpg")
+        self.notebook = Notebook.objects.create(
+            category=self.category,
+            title="Test Notebook",
+            slug="test-slug",
+            image=image,
+            price=Decimal('20000.00'),
+            diagonal='15',
+            display_type="IPS",
+            processor_freq='2.5 GHz',
+            ram="8 GB",
+            video='GeForce GTX',
+            time_without_charge='15 hours'
+        )
+        self.customer = Customer.objects.create(user=self.user, phone='123456789', address='Kyiv')
+        self.cart = Cart.objects.create(owner=self.customer)
+        self.cart_product = CartProduct.objects.create(
+            user=self.customer,
+            cart=self.cart,
+            content_object=self.notebook
+        )
+
+    def test_add_to_cart(self):
+        self.cart.products.add(self.cart_product)
+        recalc_cart(self.cart)
+        self.assertIn(self.cart_product, self.cart.products.all())
+        self.assertEqual(self.cart.products.count(), 1)
+        self.assertEqual(self.cart.final_price, Decimal('20000.00'))
+
+    def test_response_from_add_to_cart_view(self):
+        """ for test comment
+        'messages.add_message(request, messages.INFO, "Товар успішно додано")'
+        in views.py"""
+        factory = RequestFactory()
+        request = factory.get('')
+        request.user = self.user
+        response = AddToCardView.as_view()(request, ct_model='notebook', slug='test-slug')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/cart/')
+
+    def test_mock_homepage(self):
+        mock_data = mock.Mock(status_code=444)
+        with mock.patch('mainapp.views.BaseView.get', return_value=mock_data) as mock_data_:
+            factory = RequestFactory()
+            request = factory.get('')
+            request.user = self.user
+            response = BaseView.as_view()(request)
+            self.assertEqual(response.status_code, 444)
+            print(mock_data_.called)
